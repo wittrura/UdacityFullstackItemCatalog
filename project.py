172e4a660bbd52f6e35e827b0727b8bc3e396bb8
vagrant <vagrant@vagrant.vm>
2017-08-19 12:00:52 +0000

    Adds remaining routes with placeholders, sets up template structure

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+# TODO request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -14,50 +15,53 @@ session = DBSession()
 @app.route('/')
 def showAllGenres():
     genres = session.query(Genre).all()
-    output = ''
-    for g in genres:
-        output += g.name
-        output += '<br><br>'
-    return output
+    # output = ''
+    # for g in genres:
+    #     output += g.name
+    #     output += '<br><br>'
+    # return output
+    return render_template('main.html', genres = genres)
 
-# to delete
-@app.route('/catalog/<int:genre_id>/')
+@app.route('/catalog/genres/<int:genre_id>/items')
 def genreListings(genre_id):
     genre = session.query(Genre).filter_by(id = genre_id).one()
     manga = session.query(Manga).filter_by(genre_id = genre_id)
     output = ''
     for m in manga:
         output += m.name
-        output += '<br>'
-        output += str(m.volumes)
-        output += '<br>'
-        output += str(m.chapters)
-        output += '<br>'
-        output += m.authors
-        output += '<br>'
-        output += m.description
         output += '<br><br>'
     return output
-#
 
-@app.route('/catalog/Snowboarding/items')
-@app.route('/catalog/Snowboarding/Snowboard')
-@app.route('/catalog/Snowboard/edit')
-@app.route('/catalog/Snowboard/delete')
-@app.route('/catalog.json')
-
-def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+@app.route('/catalog/titles/<int:manga_id>')
+def mangaView(manga_id):
+    # genre = session.query(Genre).filter_by(id = genre_id).one()
+    manga = session.query(Manga).filter_by(id = manga_id).one()
     output = ''
-    for i in items:
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br><br>'
+    output += manga.name
+    output += '<br><br>'
+    output += manga.description
+    output += '<br><br>'
+    output += str(manga.volumes)
+    output += '<br><br>'
+    output += str(manga.chapters)
+    output += '<br><br>'
+    output += manga.authors
+    output += '<br><br>'
     return output
+
+@app.route('/catalog/titles/<int:manga_id>/edit')
+def mangaEdit(manga_id):
+    output = 'THIS IS THE ROUTE FOR EDITING TITLES'
+    return output
+
+@app.route('/catalog/titles/<int:manga_id>/delete')
+def mangaDelete(manga_id):
+    output = 'THIS IS THE ROUTE FOR DELETING TITLES'
+    return output
+
+@app.route('/catalog/json')
+def catalogJSON():
+    return 'THIS IS THE ROUTE FOR RETURING DB DATA IN JSON FORMAT'
 
 if __name__ == '__main__':
     app.debug = True
